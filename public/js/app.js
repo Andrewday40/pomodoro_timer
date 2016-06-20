48c839ffd8336b08fee5aa2c5e13c2527af6454e
Andrew <andrewday40@gmail.com>
2016-06-20 19:55:42 -0400

    created seconds countdown

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,23 @@
-console.log('My Javascript is working...!!!');
+
+$(document).ready(function(){
+  var start = $('#start');
+  // The $ before start refers that jquery is being used there, and needs to be at the beginning of every start.
+  var minutes = $('#minutes');
+  var seconds = $('#seconds');
+
+  start.on('click', startCountdown);
+
+  function startCountdown(){
+    setInterval(function(){
+       var secondsVal = +seconds.text(); //the plus makes this use numbers
+      if(secondsVal === 0){
+        seconds.text(59);
+      } else {
+        seconds.text(secondsVal - 1);
+      }
+
+    }, 1000);
+  }
+
+
+});
