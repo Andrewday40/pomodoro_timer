032801d12b20fd5f52d03b35db44f0954ef0e078
Andrew <andrewday40@gmail.com>
2016-06-23 19:38:40 -0400

    changed request and response in index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var port = process.env.PORT || 8080;
 
 server.use(express.static(__dirname+'/public'));
 
-server.get('/', function(response, request){
+server.get('/', function(request, response){
   response.sendFile('public/html/index.html', {root: __dirname});
 });
 
